1fc62ed5e422aa5d48d68083054cdb743413f8ba
Felipe César Silveira de Assis <fel.cesar@gmail.com>
2019-03-30 14:34:12 -0300

    feat: Initial bitcoin price model

--- Cipher Price Index/Model/BitcoinPrice.swift
+++ Cipher Price Index/Model/BitcoinPrice.swift
@@ -7,6 +7,18 @@
 //
 import Foundation
 
-class BitcoinPrice {
+class BitcoinPrice: Codable {
+  let code: String
+  let coinDescription: String
+  let rate:String
+  let rate_float: Double
+  let date: Date
 
+  init(code: String, coinDescription: String, rate:String, rate_float: Double, date: Date) {
+    self.code = code;
+    self.coinDescription = coinDescription
+    self.rate = rate
+    self.rate_float = rate_float
+    self.date = date
+  }
 }
