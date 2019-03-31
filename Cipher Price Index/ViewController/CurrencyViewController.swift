c934c13e0eb5d5a568528f99b87ef118a542fad4
Felipe César Silveira de Assis <fel.cesar@gmail.com>
2019-03-31 11:47:30 -0300

    feat: First implementation of currecy select view controller

--- Cipher Price Index/ViewController/CurrencyViewController.swift
+++ Cipher Price Index/ViewController/CurrencyViewController.swift
@@ -7,16 +7,77 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class CurrencyViewController: UIViewController {
+class CurrencyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+  @IBOutlet weak var searchBar: UISearchBar!
+  @IBOutlet weak var tableView: UITableView!
+
+  let disposeBag = DisposeBag()
+
+  var currencies: [Currency] = []
+  var currenciesDisplay: [Currency] = []
+
+  override func viewDidLoad() {
+
+    super.viewDidLoad()
+    tableView.dataSource = self
+    tableView.delegate = self
+    tableView.keyboardDismissMode = .onDrag
+
+    let apiErrorClosure: (Error) -> Void = { error in
+      switch error {
+      case ApiError.forbidden:
+        print("Forbidden error")
+      case ApiError.notFound:
+        print("Not found error")
+      default:
+        print("Unknown error:", error)
+      }
+    }
+
+    CoinDeskAPI.getSupportedCurrencies().observeOn(MainScheduler.instance)
+      .subscribe(onNext: { currency in
+        print(currency.country)
+        self.currencies.append(currency)
+      }, onError: apiErrorClosure, onCompleted: {
+        self.currenciesDisplay = self.currencies
+        self.tableView.reloadData()
+      })
+      .disposed(by: disposeBag)
+
+
+    searchBar.rx.text.orEmpty
+      .subscribe(onNext: { [unowned self] query in
+        self.currenciesDisplay = self.currencies.filter({ (currency) -> Bool in
+          return (currency.code.hasPrefix(query) || currency.country.hasPrefix(query))
+        })
+        self.tableView.reloadData()
+      })
+      .disposed(by:disposeBag)
     }
     
+  // MARK: UITableViewDataSource
 
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return currenciesDisplay.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cityPrototypeCell", for: indexPath)
+    cell.textLabel?.text = "\(self.currenciesDisplay[indexPath.row].country)"
+    return cell
+  }
+
+  // MARK: UITableViewDelegate
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    UserDefaults.standard.set(self.currenciesDisplay[indexPath.row].code, forKey: "SelectedCurrency")
+    self.dismiss(animated: true, completion: nil)
+  }
     /*
     // MARK: - Navigation
 
