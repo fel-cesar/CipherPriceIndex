e9cd575d4db58abe644790f8efc57ef4917ce8d5
Felipe César Silveira de Assis <fel.cesar@gmail.com>
2019-03-31 04:15:58 -0300

    chore: Add models for Currency

---
+++ Cipher Price Index/Model/Currency.swift
@@ -0,0 +1,13 @@
+//
+//  Currency.swift
+//  Cipher Price Index
+//
+//  Created by Felipe César Silveira de Assis on 31/03/19.
+//  Copyright © 2019 Cipher Studio. All rights reserved.
+//
+
+import UIKit
+
+class Currency: NSObject {
+
+}
---
+++ Cipher Price Index/ModelView/CurrencyView.swift
@@ -0,0 +1,13 @@
+//
+//  CurrencyView.swift
+//  Cipher Price Index
+//
+//  Created by Felipe César Silveira de Assis on 31/03/19.
+//  Copyright © 2019 Cipher Studio. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyView: NSObject {
+
+}
